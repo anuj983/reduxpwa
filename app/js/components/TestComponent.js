4947a017a2860d75cb2bcbef05653b6615529788
anuj983 <anuj.agrawal@99acres.com>
2017-03-06 20:29:03 +0530

    readme

--- app/js/components/TestComponent.js
+++ app/js/components/TestComponent.js
@@ -8,7 +8,17 @@ class TestComponent extends React.Component {
     super(props);//calling this makes this.props = props else this.props = undefined
     console.log(this.props);
   }
+  componentWillMount() {
+    console.log("component Will mount - before render");
+  }
+  componentDidMount() {
+    console.log("component did mount - after render");
+  }
+  componentWillUnmount() {
+    console.log("component did Unmount");
+  }
   render() {
+    console.log("rendering");
     return <div>Hello world - component text {this.props.testprop}</div>;
   }
 }
